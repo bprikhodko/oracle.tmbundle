8ba2bf5419da89e3f6bacc29a62f37f5b5916cba
Приходько Борис <bprikhodko@gmail.com>
2015-08-25 12:27:29 +0600

    Исправлены сниппеты

--- deploy.bash
+++ deploy.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 SUBLIME_HOME="/c/tools/Sublime Text Build 3083 x64"
 rm -r -f  "$SUBLIME_HOME/Data/Cache"
+rm -r -f  "$SUBLIME_HOME/Data/Installed Packages/Oracle PL SQL.sublime-package"
 zip -r    "$SUBLIME_HOME/Data/Installed Packages/Oracle PL SQL.sublime-package" Commands/
 zip -r    "$SUBLIME_HOME/Data/Installed Packages/Oracle PL SQL.sublime-package" DragCommands/
 zip -r    "$SUBLIME_HOME/Data/Installed Packages/Oracle PL SQL.sublime-package" Preferences/
